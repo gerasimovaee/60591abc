dbd030d45f4aadc843bf76743e2f4d8a545c3be4
Елена <gerasimova_ee@edu.surgu.ru>
2022-12-04 23:40:13 +0500

    Lesson_1_db

--- catalog.php
+++ catalog.php
@@ -1,6 +1,6 @@
 <?php
-require('abconnect.php');
-$result = $conn->query ("SELECT * FROM products");
+require('dbconnect2.php');
+$result = $conn->query("SELECT * FROM products");
 require('components/header.php');
 require('components/products_list.php');
 require('components/footer.php');
