70b7da477fd52d5fc4bd3ec9958bbf2d35c8e177
Елена <gerasimova_ee@edu.surgu.ru>
2023-02-24 19:38:52 +0500

    auth

--- components/header.php
+++ components/header.php
@@ -5,6 +5,7 @@
     <title>Интернет-магазин товаров для дома</title>
     <link href="style.css" rel="stylesheet">
 </head>
+
 <body>
 <header class="site-header">
     <nav class="site-navigation">
@@ -12,11 +13,21 @@
             <img src="img/logo-full.svg" width="62" height="17" alt="Логотип магазина gloevk">
         </a>
         <ul class="navigation-list">
-            <li><a href="catalog.php">Каталог</a></li>
             <li><a href="index.php?page=catalog">Каталог</a></li>
             <li><a href="delivery.html">Доставка</a></li>
             <li><a href="contacts.html">Контакты</a></li>
             <li><a href="blog.html">Блог</a></li>
+                        <!--            <li><a href="blog.html">Блог</a></li>-->
+            <?php
+            if ($_SESSION['login']) {
+//                echo ('<li><a href="product_add.php">Добавить продукт</a></li>');
+                echo ('<li>'.$_SESSION['login'].'<a href="index.php?logout=1">Выйти</a></li>');
+//                echo ('<li><a href="order.php">Заказ</a></li>');
+            }
+            else {
+                echo (' <li><a href="index.php?page=login">Войти</a></li>');
+            }
+            ?>
         </ul>
     </nav>
 </header>
