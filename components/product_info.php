56e258695bd08375fbc6dae4f012a99e489107e5
Елена <gerasimova_ee@edu.surgu.ru>
2023-03-24 19:08:25 +0500

    create_product

--- components/product_info.php
+++ components/product_info.php
@@ -1,8 +1,13 @@
 <section class="item">
+    <?
+    $id = $_GET['product_id'];
+    $result = $conn->query("SELECT * FROM products WHERE id=".$id);
+    $item = $result->fetch() ?>
     <div class="container">
         <h1 class="item-title"><?= $item['name'] ?></h1>
         <img src="<?= $item['image_URL'] ?>" width="510" height="392" alt="<?= $item['name'] ?>">
         <p class="price"><del><?= $item['price']*1.1 ?></del> <?= $item['price'] ?></p>
         <p><?= $item['description'] ?></p>
+
     </div>
 </section>
